cbdaa92ba8a322a646f1384bb38dc03f81d72595
liran <lr154499@alibaba-inc.com>
2018-04-15 21:08:52 +0800

    flag if all data send finish when send_data_done callback

--- src/log_producer_sender.c
+++ src/log_producer_sender.c
@@ -96,6 +96,7 @@ void * log_producer_send_thread(void * param)
         void * send_param = log_queue_pop(producer_manager->sender_data_queue, 30);
         if (send_param != NULL)
         {
+            ATOMICINT_INC(producer_manager->multi_thread_send_count);
             log_producer_send_fun(send_param);
         }
     }
@@ -109,6 +110,8 @@ void * log_producer_send_fun(void * param)
     if (send_param->magic_num != LOG_PRODUCER_SEND_MAGIC_NUM)
     {
         aos_fatal_log("invalid send param, magic num not found, num 0x%x", send_param->magic_num);
+        log_producer_manager * producer_manager = (log_producer_manager *)send_param->producer_manager;
+        ATOMICINT_DEC(producer_manager->multi_thread_send_count);
         return NULL;
     }
 
@@ -126,6 +129,7 @@ void * log_producer_send_fun(void * param)
             // @debug
             //continue;
             aos_info_log("send fail but shutdown signal received, force exit");
+            ATOMICINT_DEC(producer_manager->multi_thread_send_count);
             break;
         }
         lz4_log_buf * send_buf = send_param->log_buf;
@@ -142,6 +146,7 @@ void * log_producer_send_fun(void * param)
                                                       config->project, config->logstore,
                                                       send_buf);
 
+        ATOMICINT_DEC(producer_manager->multi_thread_send_count);
         int32_t sleepMs = log_producer_on_send_done(send_param, rst, &error_info);
 
         post_log_result_destroy(rst);
@@ -196,10 +201,26 @@ int32_t log_producer_on_send_done(log_producer_send_param * send_param, post_log
         log_producer_result callback_result = send_result == LOG_SEND_OK ?
                                               LOG_PRODUCER_OK :
                                               (LOG_PRODUCER_SEND_NETWORK_ERROR + send_result - LOG_SEND_NETWORK_ERROR);
+        //check if main queue still have data not send out
+        int send_finished = 0;
         uint64_t send_queue_size = producer_manager->send_param_queue_write - producer_manager->send_param_queue_read;
         int32_t log_group_size = log_queue_size(producer_manager->loggroup_queue);
+        if (send_queue_size == 0 && log_group_size == 0)
+        {
+          send_finished = 1;
+        }
 
-        producer_manager->send_done_function(producer_manager->producer_config->logstore, callback_result, send_param->log_buf->raw_length, send_param->log_buf->length, result->requestID, result->errorMessage, send_queue_size, log_group_size, producer_manager->user_param);
+        //check if mutil thread still have date not send out
+        if (send_finished == 1 && producer_manager->sender_data_queue != NULL)
+        {
+          int32_t send_data_queue_size = log_queue_size(producer_manager->sender_data_queue);
+          if (send_data_queue_size != 0 || producer_manager->multi_thread_send_count != 0)
+          {
+            send_finished = 0;
+          }
+        }
+
+        producer_manager->send_done_function(producer_manager->producer_config->logstore, callback_result, send_param->log_buf->raw_length, send_param->log_buf->length, result->requestID, result->errorMessage, send_finished, producer_manager->user_param);
     }
     switch (send_result)
     {
