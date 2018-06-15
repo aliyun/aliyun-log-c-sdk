64c452cf70e265deea1194678e768ebfdc8916ca
liran <lr154499@alibaba-inc.com>
2018-06-15 14:12:27 +0800

    remove compiler x86

--- scripts/build-ios.sh
+++ scripts/build-ios.sh
@@ -6,7 +6,6 @@ cd ../projects/iOS
 TARGET="aliyun_log_c_sdk"
 Manager="libaliyun_log_c_sdk"
 BUILD_DIR=`pwd`/"build"
-X86_DIR=$BUILD_DIR/"x86"
 ARM_DIR=$BUILD_DIR/"arm"
 if [ -d $BUILD_DIR ]; then
     rm -r $BUILD_DIR
@@ -14,10 +13,8 @@ fi
 mkdir $BUILD_DIR
 xcodebuild -project ./$TARGET.xcodeproj -scheme $TARGET VALID_ARCHS="armv7 arm64" -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=$ARM_DIR
 
-xcodebuild -project ./$TARGET.xcodeproj -scheme $TARGET VALID_ARCHS="i386 x86_64" -configuration Release -sdk iphonesimulator CONFIGURATION_BUILD_DIR=$X86_DIR
-lipo -create $ARM_DIR/$Manager.a  $X86_DIR/$Manager.a -output $BUILD_DIR/libaliyun_log_c_sdk.a
+cp $ARM_DIR/$Manager.a $BUILD_DIR/libaliyun_log_c_sdk.a
 
-rm -r $X86_DIR
 rm -r $ARM_DIR
 rm -r src
 
