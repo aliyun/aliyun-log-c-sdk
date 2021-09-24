05a81aa01d520598a7c7e02be03814c05a80a2e2
davidzhang.zc <davidzhang.zc@alibaba-inc.com>
2021-09-24 18:18:06 +0800

    [fix] crash caused by too large log size

--- src/inner_log.c
+++ src/inner_log.c
@@ -32,16 +32,37 @@ void aos_log_format(int level,
 {
     va_list args;
     char buffer[1024];
+    int maxLen = 1020;
 
-    int len = snprintf(buffer, 1020, "[%s] [%s:%d] ",
+    // @note return value maybe < 0 || > maxLen
+    int len = snprintf(buffer, maxLen, "[%s] [%s:%d] ",
                    _aos_log_level_str[level],
                     file, line);
+    // should never happen
+    if (len < 0 || len > maxLen) {
+        puts("[aos_log_format] error log fmt\n");
+        return;
+    }
     
     va_start(args, fmt);
-    len += vsnprintf(buffer + len, 1020 - len, fmt, args);
+    // @note return value maybe < 0 || > maxLen
+    int rst = vsnprintf(buffer + len, maxLen - len, fmt, args);
     va_end(args);
+    if (rst < 0) {
+        puts("[aos_log_format] error log fmt\n");
+        return;
+    }
 
-    while (buffer[len -1] == '\n') len--;
+    if (rst > maxLen - len) {
+        rst = maxLen - len;
+    }
+    len += rst;
+
+
+    while (len > 0 && buffer[len -1] == '\n')
+    {
+        len--;
+    }
     buffer[len++] = '\n';
     buffer[len] = '\0';
 
