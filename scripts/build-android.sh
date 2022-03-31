2557e8717328f4a04a6cb7997be34b36f3e81aff
gzbleu <yulong.gyl@alibaba-inc.com>
2022-03-31 15:35:01 +0800

    feat: android support armeabi

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -21,10 +21,12 @@ fi
 mkdir -p $OUTPUT_DIR
 cp -r ./projects/android/obj/local/arm64-v8a $OUTPUT_DIR/
 cp -r ./projects/android/obj/local/armeabi-v7a $OUTPUT_DIR/
+cp -r ./projects/android/obj/local/armeabi $OUTPUT_DIR/
 cp -r ./projects/android/obj/local/x86 $OUTPUT_DIR/
 cp -r ./projects/android/obj/local/x86_64 $OUTPUT_DIR/
 rm -rf $OUTPUT_DIR/arm64-v8a/objs
 rm -rf $OUTPUT_DIR/armeabi-v7a/objs
+rm -rf $OUTPUT_DIR/armeabi/objs
 rm -rf $OUTPUT_DIR/x86/objs
 rm -rf $OUTPUT_DIR/x86_64/objs
 rm -rf ./projects/android/obj
